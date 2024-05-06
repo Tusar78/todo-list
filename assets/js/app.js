f81cc8f6a6045c9e869efff01214eb82eaeba130
Tusar <tusarahmed141@gmail.com>
2024-05-06 17:29:55 +0600

    listing some of necessary references

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,14 @@ const getId = (elem) => {
   return document.getElementById(elem);
 };
 
-const inputField = getId("input-field");
-const taskList = getId("task-list");
-const addTodo = getId("addTodo");
+// All of References
+const inputField = getId('input-filed');
+const addBtn = getId('push');
 
-// console.log(inputField, taskList)
-const getD = localStorage.getItem("todo");
-
-const data = getD ? JSON.parse(getD) : [];
-
-addTodo.addEventListener("click", (e) => {
-    let mydata = { name: inputField.value };
-    data.push(mydata);
-    console.log(data);
-    localStorage.setItem("todo", JSON.stringify(data));
-    let p = document.createElement("p");
-    p.innerText = inputField.value;
-    taskList.append(p);
-});
+const tasks = getId('tasks');
 
 
-data.forEach((e) => {
-    let p = document.createElement("p");
-    p.innerText = e.name;
-    taskList.append(p);
-});
+const todoList = () => {
+    console.log('hello');
+}
+addBtn.addEventListener('click', todoList);
